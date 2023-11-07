3799d1e642b2c105918d1d67480b245a849446e6
Jeremiah Bolden <Jeremiah791@revature.net>
2023-11-07 09:39:59 -0500

    second push

---
+++ MartiallawApplication.java
@@ -0,0 +1,14 @@
+package com.martial.law.jeremiah.martiallaw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MartiallawApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(MartiallawApplication.class, args);
+	}
+
+}
